08c314a9c5e0b20aa0a76fd07f1612170e7c075e
Jan Schulte <jan@epsagon.com>
2021-05-27 10:48:48 -0400

    Deploy epsagon layer on function

--- lib/install-epsagon-lambda-layer-stack.ts
+++ lib/install-epsagon-lambda-layer-stack.ts
@@ -1,9 +1,32 @@
 import * as cdk from '@aws-cdk/core';
+import * as apigateway from "@aws-cdk/aws-apigateway";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as path from 'path';
 
 export class InstallEpsagonLambdaLayerStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const epsagonLayer = lambda.LayerVersion.fromLayerVersionAttributes(this, "EpsagonInstrumentation", {
+      layerVersionArn: "arn:aws:lambda:us-west-1:066549572091:layer:epsagon-node-layer:305"
+    });
+
+    const handler = new lambda.Function(this, "EpsagonInstrumentationHandler", {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.fromAsset("resources"),
+      handler: "handler.main",
+      layers: [epsagonLayer]
+    });
+
+    const api = new apigateway.RestApi(this, "epsagon-api", {
+      restApiName: "Epsagon Instrumentation Example",
+      description: "This is an example on how to install a Lambda Layer programmatically"
+    });
+
+    const handlerIntegration = new apigateway.LambdaIntegration(handler, {
+      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+    });
+
+    api.root.addMethod("GET", handlerIntegration); // GET /
   }
 }
